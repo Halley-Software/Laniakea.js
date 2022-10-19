ac8eb031360e42e8a2278eebd329ae4ef97fc361
Raxabi <beltrandiaztorres@gmail.com>
2022-10-19 19:43:02 +0000

    [FIX] Fixed types export path

--- packages/halley.js/lib/index.ts
+++ packages/halley.js/lib/index.ts
@@ -5,17 +5,17 @@
 'use strict';
 
 /**
- * Core Componentes of Halley
+ * Core and Utils Components of Halley
  */
 export { Halley } from "./core/halley.js";
 export { HRouter } from "./core/router/halley.router.js"
 export { Request } from "./core/request.js";
 export { Reply } from "./core/reply.js";
+export { HColors } from "./utils/halley.colors.js";
 
 /**
  * Core types of Halley
  */
-export type { HalleyEnvironment, HalleyListener } from "./HalleyTypes/Halley.types.js";
-export { Route } from "./HalleyTypes/Router.types.js";
-export type { body } from "./HalleyTypes/Reply.types.js";
-export { HColors } from "./utils/halley.colors.js";
+export type { HalleyEnvironment, HalleyListener } from "./types/Halley.types.js";
+export { Route } from "./types/Router.types.js";
+export type { body } from "./types/Reply.types.js";
